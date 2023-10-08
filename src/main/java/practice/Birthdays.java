b41713f0ad2c12ecc688191d548ba5d1e9bf2a73
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2023-10-09 00:09:08 +0300

    homework 8 done

--- src/main/java/practice/Birthdays.java
+++ src/main/java/practice/Birthdays.java
@@ -1,23 +1,37 @@
 package practice;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Birthdays {
 
     public static void main(String[] args) {
 
-        int day = 31;
-        int month = 12;
-        int year = 1990;
+        int day = 19;
+        int month = 07;
+        int year = 1994;
 
         System.out.println(collectBirthdays(year, month, day));
 
     }
 
     public static String collectBirthdays(int year, int month, int day) {
+        LocalDate birthday = LocalDate.of(year, month, day);
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy - E").localizedBy(new Locale("US"));
+        int age = 0;
+        String info = "";
+        while (!birthday.isAfter(today)) {
+            info += System.lineSeparator() + age + " - " + formatter.format(birthday);
+            birthday = birthday.plusYears(1);
+            age ++;
+        }
 
         //TODO реализуйте метод для построения строки в следующем виде
         //0 - 31.12.1990 - Mon
         //1 - 31.12.1991 - Tue
         
-        return "";
+        return info;
     }
 }
--- src/main/java/practice/TrucksAndContainers.java
+++ src/main/java/practice/TrucksAndContainers.java
@@ -10,6 +10,32 @@ public class TrucksAndContainers {
         //получение количество коробок от пользователя
         int boxes = scanner.nextInt();
 
+        int maxBoxesPerContainer = 27;
+        int maxContainersInTruck = 12;
+        int containers = 0;
+        int trucks = 0;
+
+
+        for(int i = 1; i <= boxes; i ++) {
+            if (i%(maxBoxesPerContainer * maxContainersInTruck) == 1) {
+                trucks ++;
+                System.out.println("Грузовик: " + trucks);
+            }
+            if (i%maxBoxesPerContainer == 1) {
+                containers++;
+                System.out.println("\tКонтейнер: " + containers);
+            }
+            if (i%maxBoxesPerContainer != 0) {
+                System.out.println("\t\tЯщик: " + i);
+            } else {
+                System.out.println("\t\tЯщик: " + 27 * containers);
+            }
+        }
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков - " + trucks + " шт.");
+        System.out.println("контейнеров - " + containers + " шт.");
+
+
         // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
         // пример вывода при вводе 2
         // для отступа используйте табуляцию - \t
